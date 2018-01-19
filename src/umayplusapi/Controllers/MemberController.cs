ed9f1a9316b8aff61598d2892bc87f5ab9cee801
tidarat <tidarat.sri@thailife.com>
2018-01-19 13:56:22 +0700

    add edit นิดหน่อย

--- src/umayplusapi/Controllers/MemberController.cs
+++ src/umayplusapi/Controllers/MemberController.cs
@@ -4,38 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace umayplusapi.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class MemberController : Controller
     {
-        // GET api/values
+        // GET: api/<controller>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/<controller>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/<controller>
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/<controller>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/<controller>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
